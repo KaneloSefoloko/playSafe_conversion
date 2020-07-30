f6dc75dc334ac85a6e317132916f797495d9070e
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-31 01:51:37 +0200

    controller

--- src/main/java/com/playsafe/demo/conversion/Worker.java
+++ src/main/java/com/playsafe/demo/conversion/Worker.java
@@ -3,22 +3,23 @@ package com.playsafe.demo.conversion;
 public class Worker implements ConversionApi{
 
     private final double KILO = 1.609;
+    private final double DEGREES = 273.15;
 
     @Override
     public float convertToCelcius(int kelvin) throws Exception {
 
 
-        return null;
+        return (float) (kelvin - DEGREES);
     }
 
     @Override
     public float convertToKelvin(int celsius) throws Exception {
-        return null;
+        return (float) (celsius + DEGREES);
     }
 
     @Override
     public double convertToMiles(int kilo) throws Exception {
-        return null;
+        return kilo / KILO;
     }
 
     @Override

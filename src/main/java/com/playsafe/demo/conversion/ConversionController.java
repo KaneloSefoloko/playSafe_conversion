50a9eb2044eeabeb5c6384fcedf5c8519274b470
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-31 01:11:59 +0200

    controller

--- src/main/java/com/playsafe/demo/conversion/ConversionController.java
+++ src/main/java/com/playsafe/demo/conversion/ConversionController.java
@@ -1,4 +1,29 @@
 package com.playsafe.demo.conversion;
 
-public class ConversionController {
+@RestController
+public class ConversionController implements ConversionApi{
+
+    @Override
+    @RequestMapping(path = "/conversions/ktoc", method = RequestMethod.GET, produces = { "application/json" })
+    public ResponseEntity<Void> convertKelvin() throws Exception {
+        return null;
+    }
+
+    @Override
+    @RequestMapping(path = "/conversions/ctoc", method = RequestMethod.GET, produces = { "application/json" })
+    public ResponseEntity<Void> convertCelsius() throws Exception {
+        return null;
+    }
+
+    @Override
+    @RequestMapping(path = "/conversions/ktom", method = RequestMethod.GET, produces = { "application/json" })
+    public ResponseEntity<Void> convertKilo() throws Exception {
+        return null;
+    }
+
+    @Override
+    @RequestMapping(path = "/conversions/mtok", method = RequestMethod.GET, produces = { "application/json" })
+    public ResponseEntity<Void> convertMiles() throws Exception {
+        return null;
+    }
 }
